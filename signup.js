d02e1cf1f9c7ef293344f473434624426b7a6e72
Joatan Carlos <joatancarlos@gmail.com>
2022-06-23 19:27:38 -0300

    melhorias

---
+++ signup.js
@@ -0,0 +1,8 @@
+let f_name = window.document.querySelector('#f-name')
+let l_name = window.document.querySelector("#l-name")
+let email = window.document.querySelector("#email")
+let password = window.document.querySelector("#password")
+
+function send() {
+    
+}

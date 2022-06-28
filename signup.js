f214d51a007a05bec9fb0c0fb6a48da85fad88c1
Joatan Carlos <joatancarlos@gmail.com>
2022-06-28 15:20:04 -0300

    aparentemente-finalizado

--- signup.js
+++ signup.js
@@ -7,9 +7,15 @@ const validateEmail = (email) => {
 
 function send() {
     var input = document.getElementsByTagName('input');
-
-    // First Name
     var divErrorFN = document.getElementById('msg-f-name');
+    var divErrorLN = document.getElementById('msg-l-name')
+    var divErrorEm = document.getElementById('msg-email');
+    var divErrorPW = document.getElementById('msg-password');
+
+
+    
+    // First Name
+    
     if (input[0].value == '') {
         divErrorFN.style.display = 'block';
         
@@ -24,9 +30,24 @@ function send() {
             input[0].classList.remove('iconError');
     }
 
+    // Last Name
+
+    if (input[1].value == '') {
+        divErrorLN.style.display = 'block';
+        
+        // exibe ícone de erro
+        if (!input[1].classList.contains('iconError'))
+            input[1].classList.add('iconError');
+    } else {
+        divErrorLN.style.display = 'none';
+
+        // remove ícone de erro
+        if (input[1].classList.contains('iconError'))
+            input[1].classList.remove('iconError');
+    }
 
     // E-mail
-    var divErrorEm = document.getElementById('msg-email');
+    
     if (validateEmail(input[2].value)) {
         divErrorEm.style.display = 'none';
 
@@ -34,13 +55,30 @@ function send() {
         if (input[2].classList.contains('iconError'))
             input[2].classList.remove('iconError');
     } else {
-        //divErrorEm.innerText = 'E-mail de marico!';
+        
         divErrorEm.style.display = 'block';
 
         // exibe ícone de erro
         if (!input[2].classList.contains('iconError'))
             input[2].classList.add('iconError');
     }
+
+    // Password
+
+    if (input[3].value == '') {
+        divErrorPW.style.display = 'block';
+        
+        // exibe ícone de erro
+        if (!input[3].classList.contains('iconError'))
+            input[3].classList.add('iconError');
+    } else {
+        divErrorPW.style.display = 'none';
+
+        // remove ícone de erro
+        if (input[3].classList.contains('iconError'))
+            input[3].classList.remove('iconError');
+    }
+
     // Recarrega a página -> retorna true
     return false;
 }
